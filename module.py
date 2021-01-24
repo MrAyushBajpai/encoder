ee3dd9b41f34b5eb9f72fa2fe1e24639962c437e
Ayush Bajpai <76244712+MrAyushBajpai@users.noreply.github.com>
2021-01-24 08:37:55 +0530

    Update module.py

--- module.py
+++ module.py
@@ -4,28 +4,62 @@ import datetime
 import time
 
 # Defining Necessary Variables
-historyfile = 'historyfile'
-logfile = 'logfile'
+keyphrase_ncode = {'a': 'm', 'b': 'w', 'c': 'e', 'd': 'r', 'e': 't', 'f': 'y', 'g': 'u', 'h': 'i', 'i': 'q', 'j': 'o',
+                   'k': 'p', 'l': 'a', 'm': 's', 'n': 'd', 'o': 'f', 'p': 'g', 'q': 'h', 'r': 'j', 's': 'k', 't': 'l',
+                   'u': 'z', 'v': 'x', 'w': 'c', 'x': 'v', 'y': 'b', 'z': 'n',
+
+                   'A': 'P', 'B': 'W', 'C': 'E', 'D': 'R', 'E': 'T', 'F': 'Y', 'G': 'U', 'H': 'I', 'I': 'M', 'J': 'O',
+                   'K': 'Q', 'L': 'A', 'M': 'S', 'N': 'D', 'O': 'F', 'P': 'G', 'Q': 'H', 'R': 'J', 'S': 'K', 'T': 'L',
+                   'U': 'Z', 'V': 'X', 'W': 'C', 'X': 'V', 'Y': 'B', 'Z': 'N',
+
+                   '1': '*', '2': '@', '3': '$', '4': '#', '5': '%', '6': '!', '7': '&', '8': '^', '9': '(', '0': ')',
+
+                   '!': '5', '@': '7', '#': '1', '$': '4', '%': '3', '^': '6', '&': '9', '*': '8', '(': '0', ')': '2',
+
+                   '`': '~', '~': '`', '[': '}', ']': '{', '}': '[', '{': ']', '-': '+', '_': '=', '=': '-', '+': '-',
+                   '\'': '\"', '\"': '\'', '.': '?', '?': '.', ',': '/', '/': ',', '<': '>', '>': '<', ':': ';',
+                   ';': ':', '|': '\\', '\\': '|',
+
+                   ' ': ' '
+                   }
+keyphrase_dcode = dict([(value, key) for key, value in keyphrase_ncode.items()])
+ncode_history = 'ncode.his'
+dcode_history = 'dcode.his'
+logfile = 'log.txt'
 
 
-# Fucntion to encrypt the data
-def encrypt(msg, key):
-    encryped = []
-    for i, c in enumerate(msg):
-        key_c = ord(key[i % len(key)])
-        msg_c = ord(c)
-        encryped.append(chr((msg_c + key_c) % 127))
-    return ''.join(encryped)
+# Function to encode the data
+def encoder(data):
+    logcat('Received ' + data + ' as the input to be process at the encoder', 'ncode', False)
+    encoded = ''
+    encode = ''
+    for item in data:
+        if item in keyphrase_ncode:
+            encoded += keyphrase_ncode.get(item)
+            encode = encoded
+
+        else:
+            encoded += item
+            encode = encoded
+    logcat('Outputing ' + encode + ' as the processed output from the encoder', 'ncode', False)
+    return encode
 
 
-# Function to Decrypt the Data
-def decrypt(enc, key):
-    msg = []
-    for i, c in enumerate(enc):
-        key_c = ord(key[i % len(key)])
-        enc_c = ord(c)
-        msg.append(chr((enc_c - key_c) % 127))
-    return ''.join(msg)
+# Function to Decode the data
+def decoder(data):
+    logcat('Received ' + data + ' as the input to be process at the decoder', 'dcode', False)
+    decoded = ''
+    decode = ''
+    for charc in data:
+        if charc in keyphrase_dcode:
+            decoded += keyphrase_dcode.get(charc)
+            decode = decoded
+
+        else:
+            decoded += charc
+            decode = decoded
+    logcat('Outputing ' + decode + ' as the processed output from the decoder', 'ncode', False)
+    return decode
 
 
 # Function to check if the file exists and if it is empty or not
@@ -58,15 +92,18 @@ def clear():
 # Function to write history to their specific history files
 def writehis(historytext, origin, towrite):
     if towrite:
-        fhistory = open(historyfile, 'a')
         if origin == 'ncode':
-            fhistory.write('Encode - ' + historytext)
-            fhistory.write('\n')
-            fhistory.close()
+            n_history = open(ncode_history, 'a')
+            n_history.write(historytext)
+            n_history.write('\n')
+            n_history.close()
+
         elif origin == 'dcode':
-            fhistory.write('Decode - ' + historytext)
-            fhistory.write('\n')
-            fhistory.close()
+            d_history = open(dcode_history, 'a')
+            d_history.write(historytext)
+            d_history.write('\n')
+            d_history.close()
+
         elif origin != 'dcode' or origin != 'ncode':
             errorhandler(1, origin)
     else:
@@ -75,25 +112,46 @@ def writehis(historytext, origin, towrite):
 
 # Function to clear specific history
 def clearhistory(origin):
-    fhistory = open(historyfile, 'w')
-    fhistory.truncate(0)
-    fhistory.close()
+    if origin == 'ncode':
+        d_history = open(ncode_history, 'w')
+        d_history.truncate(0)
+        d_history.close()
 
+    elif origin == 'dcode':
+        d_history = open(dcode_history, 'w')
+        d_history.truncate(0)
+        d_history.close()
+
+    elif origin != 'dcode' or origin != 'ncode':
+        errorhandler(1, origin)
     logcat('Cleared History', origin, False)
 
 
 # Function to Load specific History and print it
 def loadhistory(origin):
-    # Doing a filecheck to check if the history file does not exist or the history file is empty
-    if filecheck(historyfile, 'ncode'):
-        fhistory = open(historyfile, 'r')
-        print("Loading History!")
-        print('\n')
-        for item in fhistory:
-            print(item)
-    else:
-        print("There is no history for encoder")
-
+    # Checking if the requested history is from Encoder or Decoder
+    if origin == 'ncode':
+        # Doing a filecheck to check if the history file does not exist or the history file is empty
+        if filecheck(ncode_history, 'ncode'):
+            ln_history = open(ncode_history, 'r')
+            print("Loading History for Encoder")
+            print('\n')
+            for item in ln_history:
+                print(item)
+        else:
+            print("There is no history for encoder")
+    elif origin == 'dcode':
+        # Doing a filecheck to check if the history file does not exist or the history file is empty
+        if filecheck(dcode_history, 'dcode'):
+            ld_history = open(dcode_history, 'r')
+            print('Loading History for Decoder')
+            print('\n')
+            for item in ld_history:
+                print(item)
+        else:
+            print('There is no history for decoder')
+    elif origin != 'dcode' or origin != 'ncode':
+        errorhandler(1, origin)
     logcat('Loaded History', origin, False)
 
 
